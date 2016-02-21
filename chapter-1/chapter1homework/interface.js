a27286138e5d8777bc6a40090a8007d072bd30aa
sammita94 <sammitachakravarti@yahoo.com>
2016-02-21 22:11:54 +0530

    Homework 1.1 completed

---
+++ chapter-1/chapter1homework/interface.js
@@ -0,0 +1,23 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+  // TODO: implement
+  db.collection('movies').insert(doc, function(err, result) {
+  	if (err) callback(err);
+  	callback(null, result);
+  })
+};
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+  db.collection('movies').find({'director': director}).sort({'title':1}).toArray(function(err, items) {
+        if (err) { console.log(err);  callback(err);}
+  	callback(null, items);
+  })
+};
